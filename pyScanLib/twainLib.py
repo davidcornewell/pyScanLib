2ee4c3a39a572d1a9d750fa6dabe7ddc91f454f2
David Cornewell <david.cornewell@gmail.com>
2019-09-19 16:40:32 -0400

    Adding code from spacemudd's webagent-scanner. multipage scanning.

--- pyScanLib/twainLib.py
+++ pyScanLib/twainLib.py
@@ -10,18 +10,16 @@ from io import BytesIO
 
 class twainLib(object):
 
-    """
-    The is main class of Twain API (Win32)
+    """The is main class of Twain API (Win32)
     """
 
     def __init__(self):
-        self.scanner = None
         self.dpi = 200  # Define for use in pixeltoInch function
         self.lastError = None
+        self.scannedImages = []
 
     def getScanners(self):
-        """
-        Get available scanner from twain module
+        """Get available scanner from twain module
         """
         self.sourceManager = twain.SourceManager(0)
         scanners = self.sourceManager.GetSourceList()
@@ -31,33 +29,24 @@ class twainLib(object):
             return None
 
     def setScanner(self, scannerName):
-        """
-        Connected to Scanner using Scanner Name
+        """Connected to Scanner using Scanner Name
 
         Arguments:
         scannerName -- Name of Scanner return by getScanners()
         """
-
         self.scanner = self.sourceManager.OpenSource(scannerName)
 
     def setDPI(self, dpi):
+        """Set DPI to selected scanner and dpi to self.dpi
         """
-        Set DPI to selected scanner and dpi to self.dpi
-        """
-    
-        if self.scanner == None:
-            raise ScannerNotSet
-        
         self.dpi = dpi
-
         self.scanner.SetCapability(
             twain.ICAP_XRESOLUTION, twain.TWTY_FIX32, float(self.dpi))
         self.scanner.SetCapability(
             twain.ICAP_YRESOLUTION, twain.TWTY_FIX32, float(self.dpi))
 
     def setScanArea(self, left=0.0, top=0.0, width=8.267, height=11.693):
-        """
-        Set Custom scanner layout to selected scanner in Inches
+        """Set Custom scanner layout to selected scanner in Inches
 
         Arguments:
         left -- Left position of scanned Image in scanner 
@@ -65,10 +54,6 @@ class twainLib(object):
         width(right) -- Width of scanned Image
         bottom(height) -- Height of scanned Image
         """
-
-        if self.scanner == None:
-            raise ScannerNotSet
-
         #((left, top, width, height) document_number, page_number, frame_number)
         width = float(width)
         height = float(height)
@@ -76,28 +61,18 @@ class twainLib(object):
         top = float(top)
         self.scanner.SetImageLayout((left, top, width, height), 1, 1, 1)
 
-    # size in inches
+    #size in inches
     def getScannerSize(self):
+        """Return Scanner Layout as Tuple (left, top, right, bottom) in Inches       
         """
-        Return Scanner Layout as Tuple (left, top, right, bottom) in Inches       
-        """
-    
-        if self.scanner == None:
-            raise ScannerNotSet
-
         return self.scanner.GetImageLayout()
 
     def setPixelType(self, pixelType):
-        """
-        Set pixelType to selected scanner
+        """Set pixelType to selected scanner
 
-        Arguments:
-        pixelType -- Pixel type - bw (Black & White), gray (Gray) and color(Colored)
+        Args:
+        pixelType: String  bw / gray / color
         """
-        
-        if self.scanner == None:
-            raise ScannerNotSet
-
         pixelTypeMap = {'bw': twain.TWPT_BW,
                         'gray': twain.TWPT_GRAY,
                         'color': twain.TWPT_RGB}
@@ -108,13 +83,19 @@ class twainLib(object):
         self.scanner.SetCapability(
             twain.ICAP_PIXELTYPE, twain.TWTY_UINT16, pixelType)
 
-    def scan(self):
+    def setDuplex(self, boolEnabled):
+        """ Set the duplex to be enabled or not.
+        :param enabled: Boolean
         """
-        Scan and return PIL object if success else return False
-        """
-        if self.scanner == None:
-            raise ScannerNotSet
+        try:
+            self.scanner.SetCapability(twain.CAP_DUPLEXENABLED, twain.TWTY_BOOL, boolEnabled)
+        except:
+            print("Couldn't set duplex")
 
+
+    def scan(self):
+        """Scan and return PIL object if success else return False
+        """
         self.scanner.RequestAcquire(0, 1)
         info = self.scanner.GetImageInfo()
         try:
@@ -126,17 +107,63 @@ class twainLib(object):
             self.lastError = str(e)
             return False
 
-    def closeScanner(self):
+    def multiScan(self):
+        """ Scan and return an array of PIL objects 
+            If no images, will return an empty array
         """
-        Destory 'self.scanner' class of twain module generated in setScanner function
+        if self.scanner == None:
+            raise ScannerNotSet
+
+        try:
+            self.scanner.RequestAcquire(0, 0) # RequestAcquire(ShowUI, ShowModal)
+        except:
+            print("RequestAcquire Error")
+
+
+        while self.next():
+            image = self.capture()
+            if not image:
+                print("Capture didnt find any images")
+
+        return self.scannedImages
+
+    def next(self):
+        try:
+            print("next()")
+            self.scanner.GetImageInfo()
+            print("image_info()")
+            return True
+        except twain.excTWCC_SEQERROR:
+            print("next() fired an exception()")
+            return False
+
+    def capture(self):
+        try:
+            print("capture()")
+            (handle, more_to_come) = self.scanner.XferImageNatively()
+        except twain.excDSTransferCancelled:
+            return None
+        self.scannedImages.append(Image.open(BytesIO(twain.DIBToBMFile(handle))))
+        twain.GlobalHandleFree(handle)
+        return True
+        
+
+    def detectBlankPages(self):
+        try:
+            self.scanner.SetCapability(twain.ICAP_AUTODISCARDBLANKPAGES, twain.TWTY_UINT16, False)
+        except twain.excTWCC_CAPUNSUPPORTED:
+            print("Blank page detection not supported")
+
+
+    def closeScanner(self):
+        """ Destory 'self.scanner' class of twain module generated in setScanner function
         """
         if self.scanner:
             self.scanner.destroy()
         self.scanner = None
 
     def scanPreview(self):
-        """
-        Show preview of image while scanning in progress.
+        """Show preview of image while scanning in progress.
         """
         raise NotImplementedError
 
@@ -150,3 +177,4 @@ class twainLib(object):
         if self.sourceManager:
             self.sourceManager.destroy()
         (self.scanner, self.sourceManager) = (None, None)
+
