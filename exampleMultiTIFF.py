c5299ce50d530286475c80c7b7cab92ee2c46131
David Cornewell <david.cornewell@gmail.com>
2019-09-19 16:42:10 -0400

    Committing an example of storing a multi page tiff

---
+++ exampleMultiTIFF.py
@@ -0,0 +1,21 @@
+from pyScanLib import pyScanLib
+
+ls = pyScanLib() # load scanner library
+devices = ls.getScanners()
+ls.setScanner(devices[0])
+
+ls.setDPI(300)
+    
+# A4 Example
+ls.setScanArea(width=8.26,height=11.693) # (left,top,width,height) in inches
+
+ls.setPixelType("color") # bw/gray/color
+
+pils = ls.multiScan()
+if len(pils) > 0:
+    pils[0].save("multiimage.tif", compression="tiff_deflate", save_all=True, append_images=pils[1:])
+else:
+    print("Error: "+ ls.lastError)
+    
+ls.closeScanner() # unselect selected scanner, set in setScanners()
+ls.close() # Destory whole class
